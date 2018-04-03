a8216983808efa8693fdc4f6755b16ebe162c293
Administrator <Administrator@PC201506151812>
2018-04-03 13:46:33 +0800

    first version

---
+++ Test/src/edu/fjut/test/Animal.java
@@ -0,0 +1,9 @@
+package edu.fjut.test;
+
+public class Animal {
+private String name;
+public void sing()
+{
+	System.out.println("¶¯Îï½ĞÉù");
+}
+}
---
+++ Test/src/edu/fjut/test/Dog.java
@@ -0,0 +1,8 @@
+package edu.fjut.test;
+
+public class Dog extends Animal {
+public void sing()
+{
+	System.out.println("I am a dog :ÍôÍôÍô");
+}
+}
